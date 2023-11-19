228615a72c39acc73c5e0e61465038fa6ef30c8e
FieryInferno <bagassetia271@gmail.com>
2023-11-19 22:01:31 +0700

    feat: homepage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -40,6 +40,37 @@ class ItemNavbar extends StatelessWidget {
   }
 }
 
+class Greeting extends StatefulWidget {
+  const Greeting({super.key});
+
+  @override
+  State<Greeting> createState() => _Greeting();
+}
+
+class _Greeting extends State<Greeting> {
+  String _greet = '';
+
+  @override
+  void initState() {
+    super.initState();
+
+    DateTime datetime = DateTime.now().toLocal();
+    int hour = datetime.hour;
+
+    if (hour > 0 && hour <= 9) _greet = 'Selamat pagi';
+    if (hour > 9 && hour <= 18) _greet = 'Selamat sore';
+    if (hour > 18) _greet = 'Selamat malam';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextWidget(
+      '$_greet, M. BAGAS SETIA',
+      weight: FontWeight.w600,
+    );
+  }
+}
+
 class Home extends StatelessWidget {
   const Home({super.key});
 
@@ -48,26 +79,52 @@ class Home extends StatelessWidget {
     return Container(
       color: Colors.white,
       child: Stack(
-        alignment: Alignment.bottomCenter,
         children: [
-          Container(
-            height: 75,
-            margin: const EdgeInsets.only(bottom: 5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                ItemNavbar(
-                  icon: Icons.home,
-                  title: 'Beranda',
-                  active: true,
-                ),
-                ItemNavbar(
-                  icon: Icons.receipt_long,
-                  title: 'Transaksi',
-                ),
-              ],
+          Positioned(
+            top: 0,
+            right: 0,
+            width: 80,
+            height: 40,
+            child: Container(
+              decoration: const BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.amber,
+                    blurRadius: 60,
+                    spreadRadius: 50,
+                  )
+                ],
+                color: Colors.amber,
+              ),
             ),
-          )
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 50, left: 12.5),
+            child: const Greeting(),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: Container(
+              height: 75,
+              margin: const EdgeInsets.only(bottom: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: const [
+                  ItemNavbar(
+                    icon: Icons.home,
+                    title: 'Beranda',
+                    active: true,
+                  ),
+                  ItemNavbar(
+                    icon: Icons.receipt_long,
+                    title: 'Transaksi',
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
